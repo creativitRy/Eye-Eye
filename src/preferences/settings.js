9655bd87fba20ce76abf01d746dbd777ec838c03
GahwonLee <creativitRy@gmail.com>
2018-03-31 14:51:00 -0500

    fixed settings

--- src/preferences/settings.js
+++ src/preferences/settings.js
@@ -1,33 +1,23 @@
 const settings = require('electron-settings');
 
-/*let $;
-
-require("jsdom").env("", function(err, window) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-
-    $ = require("jquery")(window);
-});*/
-
 loadSettings();
 
 function loadSettings() {
-    document.getElementById("useVideo").setAttribute("value", settings.get("useVideo", false));
+    if (settings.get("useVideo", false))
+        document.getElementById("useVideo").checked = true;
     document.getElementById("videoLink").setAttribute("value", settings.get("videoLink", ""));
     document.getElementById("minTime").setAttribute("value", settings.get("minTime", 45));
     document.getElementById("maxTime").setAttribute("value", settings.get("maxTime", 90));
     console.log("loaded")
 }
 
-document.getElementById("useVideo").addEventListener("input", saveSettings);
+document.getElementById("useVideo").addEventListener("change", saveSettings);
 document.getElementById("videoLink").addEventListener("input", saveSettings);
 document.getElementById("minTime").addEventListener("input", saveSettings);
 document.getElementById("maxTime").addEventListener("input", saveSettings);
 
 function saveSettings() {
-    settings.set("useVideo", document.getElementById("useVideo").value);
+    settings.set("useVideo", document.getElementById("useVideo").checked);
     settings.set("videoLink", document.getElementById("videoLink").value);
     settings.set("minTime", document.getElementById("minTime").value);
     settings.set("maxTime", document.getElementById("maxTime").value);
