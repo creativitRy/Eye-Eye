539e916ca9dc3cbc417429f4e7517d82a918c24e
GahwonLee <creativitRy@gmail.com>
2018-03-31 14:22:17 -0500

    settings persist

--- src/preferences/settings.js
+++ src/preferences/settings.js
@@ -13,19 +13,23 @@ require("jsdom").env("", function(err, window) {
 
 loadSettings();
 
-function loadSettings()
-{
+function loadSettings() {
     document.getElementById("useVideo").setAttribute("value", settings.get("useVideo", false));
     document.getElementById("videoLink").setAttribute("value", settings.get("videoLink", ""));
     document.getElementById("minTime").setAttribute("value", settings.get("minTime", 45));
     document.getElementById("maxTime").setAttribute("value", settings.get("maxTime", 90));
-    console.log("test")
+    console.log("loaded")
 }
 
-function saveSettings()
-{
+document.getElementById("useVideo").addEventListener("input", saveSettings);
+document.getElementById("videoLink").addEventListener("input", saveSettings);
+document.getElementById("minTime").addEventListener("input", saveSettings);
+document.getElementById("maxTime").addEventListener("input", saveSettings);
+
+function saveSettings() {
     settings.set("useVideo", document.getElementById("useVideo").value);
     settings.set("videoLink", document.getElementById("videoLink").value);
     settings.set("minTime", document.getElementById("minTime").value);
     settings.set("maxTime", document.getElementById("maxTime").value);
+    console.log("saved")
 }
